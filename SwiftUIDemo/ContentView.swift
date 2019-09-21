6b8bcbbb313c170e3a017bb65e34ed26033ceabe
luoquan <nchkdxlq.gmail.com>
2019-09-21 13:19:06 +0800

    init

---
+++ SwiftUIDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by Knox on 2019/9/21.
+//  Copyright © 2019 Knox. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
